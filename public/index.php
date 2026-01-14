9db8f7007712fcfd51922a3b3185501d012eb16c
Hexandroid <kalilzie26@gmail.com>
2026-01-14 20:54:12 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 // Point d'entrée principal de l'application
 
-require_once __DIR__ . '/../routes/web.php';
-echo "e-Santé CI fonctionne sur Render 🎉";
-exit;
+require_once __DIR__ . '/../views/home.php';
 ?>
